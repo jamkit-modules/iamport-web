ffd0736303b751a978d1725be86f31745f2a7e6f
Hanyeol Cho <hanyeol.cho@gmail.com>
2021-02-19 02:20:12 +0900

    Initial commit

--- cleanup.js
+++ cleanup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var cleanup_style = document.createElement('style');
 
     cleanup_style.innerHTML = `
-        .css-p018rk-CurrentProfile,
+        .unknown,
         #unknown {
             display: none !important;
         }
